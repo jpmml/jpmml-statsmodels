02777b11b792ec3a8a589cdbb7a44b0a2d01f03d
Villu Ruusmann <villu.ruusmann@gmail.com>
2022-12-21 18:12:53 +0200

    Updated test resources

--- pmml-statsmodels/src/test/resources/main.py
+++ pmml-statsmodels/src/test/resources/main.py
@@ -14,8 +14,8 @@ def split_csv(df):
 def store_csv(df, name):
 	df.to_csv("csv/" + name + ".csv", index = False)
 
-def store_pkl(result, name):
-	result.save("pkl/" + name + ".pkl", remove_data = True)
+def store_pkl(results, name):
+	results.save("pkl/" + name + ".pkl", remove_data = True)
 
 audit_df = load_csv("Audit")
 print(audit_df.dtypes)
@@ -25,12 +25,12 @@ audit_X, audit_y = split_csv(audit_df)
 audit_formula = "Adjusted ~ Age + C(Employment) + C(Education) + C(Marital) + C(Occupation) + Income + Hours"
 
 def build_audit(model, name):
-	result = model.fit()
-	print(result.summary())
+	results = model.fit()
+	print(results.summary())
 
-	store_pkl(result, name)
+	store_pkl(results, name)
 
-	adjusted_proba = DataFrame(result.predict(audit_X), columns = ["probability(1)"])
+	adjusted_proba = DataFrame(results.predict(audit_X), columns = ["probability(1)"])
 	adjusted_proba["probability(0)"] = 1 - adjusted_proba["probability(1)"]
 	store_csv(adjusted_proba, name)
 
@@ -42,12 +42,12 @@ print(iris_df.dtypes)
 iris_X, iris_y = split_csv(iris_df)
 
 def build_iris(model, name):
-	result = model.fit()
-	print(result.summary())
+	results = model.fit()
+	print(results.summary())
 
-	store_pkl(result, name)
+	store_pkl(results, name)
 
-	species_proba = result.predict(iris_X)
+	species_proba = results.predict(iris_X)
 	species_proba.columns = ["probability(setosa)", "probability(versicolor)", "probability(virginica)"]
 	store_csv(species_proba, name)
 
@@ -61,17 +61,18 @@ auto_X, auto_y = split_csv(auto_df)
 auto_formula = "mpg ~ C(cylinders) + displacement + horsepower + weight + acceleration + C(model_year) + C(origin)"
 
 def build_auto(model, name):
-	result = model.fit()
-	print(result.summary())
+	results = model.fit()
+	print(results.summary())
 
-	store_pkl(result, name)
+	store_pkl(results, name)
 
-	mpg = DataFrame(result.predict(auto_X), columns = ["mpg"])
+	mpg = DataFrame(results.predict(auto_X), columns = ["mpg"])
 	store_csv(mpg, name)
 
 build_auto(OLS(auto_y, auto_X), "OLSAuto")
-build_auto(ols(formula = auto_formula, data = auto_df), "OLSFormulaAuto")
 build_auto(WLS(auto_y, auto_X), "WLSAuto")
+
+build_auto(ols(formula = auto_formula, data = auto_df), "OLSFormulaAuto")
 build_auto(wls(formula = auto_formula, data = auto_df), "WLSFormulaAuto")
 
 visit_df = load_csv("Visit")
@@ -82,12 +83,12 @@ visit_X, visit_y = split_csv(visit_df)
 visit_formula = "docvis ~ C(edlevel) + C(outwork) + C(female) + C(married) + C(kids) + hhninc + educ + C(self)"
 
 def build_visit(model, name):
-	result = model.fit()
-	print(result.summary())
+	results = model.fit()
+	print(results.summary())
 
-	store_pkl(result, name)
+	store_pkl(results, name)
 
-	docvis = DataFrame(result.predict(visit_X), columns = ["docvis"])
+	docvis = DataFrame(results.predict(visit_X), columns = ["docvis"])
 	store_csv(docvis, name)
 
 build_visit(poisson(formula = visit_formula, data = visit_df), "PoissonFormulaVisit")
